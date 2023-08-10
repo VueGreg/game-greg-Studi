bf1f3a38f100eabf8d9270c4ed60621963857ba7
Gregory Wolff <sharran57@gmail.com>
2023-08-10 15:58:06 +0200

    Add hold function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// -------------Variables
+
 let iterations = 9
 let diceValue
 
 const currentScore = document.querySelector('.current__score-player')
+const globalScore = document.querySelector('[data-js="score-player-one"]')
 
 const rollDice = document.querySelector('[data-js="roll-dice"]')
+const hold = document.querySelector('[data-js="hold"]')
 
 const one = document.querySelector('[data-js="middle"]')
 
@@ -43,7 +47,9 @@ const six = [
 ]
 
 const diceFace = [one, two, three, four, five, six]
+// -------------------------End variable
 
+// ------------------------Event
 rollDice.addEventListener('click', () => {
     iterations = 9
     diceRoll()
@@ -55,19 +61,28 @@ rollDice.addEventListener('click', () => {
     }, 1100)
 })
 
+hold.addEventListener('click', () => {
+    let gScore = Number(globalScore.textContent)
+    let actualScore = Number(currentScore.textContent)
+    globalScore.textContent = addScore(gScore, actualScore)
+})
+// ------------------------End event
+
+
+// -----------------------Functions
 function aleatoire(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function clean() {
-    const cleanning = document.querySelectorAll('.clean')
+function clean(selector) {
+    const cleanning = document.querySelectorAll(selector)
     cleanning.forEach(element => {
         element.style.visibility = 'hidden'
     })
 }
 
 function diceRoll() {
-    clean()
+    clean('.clean')
     let aleaNumber = aleatoire(0, 5)
     
     for (let i = 0; i < diceFace.length; i++) {
